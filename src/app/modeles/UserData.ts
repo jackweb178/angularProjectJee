4e9a283c27354ef3548fcd188139bec777481bc2
jackweb <jackweb178@gmail.com>
2020-08-17 05:14:50 +0000

    listes des users.

---
+++ src/app/modeles/UserData.ts
@@ -0,0 +1,14 @@
+export class UserData {
+
+    constructor (
+        public id : BigInteger,
+        public name: string,
+        public username:string,
+        public tel:BigInteger,
+        public email:string,
+        public matricule:string,
+        public role:BigInteger
+    ){
+
+    }
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiService = "http://localhost:8080";
+  constructor(private http: HttpClient) { }
+
+  /**
+   * liste des users
+   */
+  public getUser() {
+    return this.http.get<any>(this.apiService+"/all");
+  }
+}
